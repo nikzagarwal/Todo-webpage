907f009a6894a92abc0940398d7e42fa17d17c4c
Nikhil Agarwal <nikhilagarwalassisi@gmail.com>
2020-10-22 18:49:26 +0530

    small change

--- controllers/todocontroller.js
+++ controllers/todocontroller.js
@@ -1,24 +1,41 @@
-var bodyParser= require('body-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
-var data = [{item:'getbook'},{item:'walk dog'},{item:'coding'}];
-var urlencodedParser= bodyParser.urlencoded({extended:false});
-module.exports = function(app){
 
-app.get('/todo',function(req,res){
-    res.render('todo',{todos:data});
+mongoose.connect('mongodb+srv://Nikhil:nikhil@cluster0-stwrp.mongodb.net/test?retryWrites=true&w=majority');
+
+var todoSchema = new mongoose.Schema({
+    item: String
 });
 
-app.post('/todo',urlencodedParser,function(req,res){
-  data.push(req.body);
- res.json(data);
+var Todo = mongoose.model('Todo', todoSchema);
 
-});
 
-app.delete('/todo/:item',function(req,res){
-    data = data.filter(function(todo){
-        return todo.item.replace(/ /g,'-') !== req.params.item;
+//var data = [{ item: 'getbook' }, { item: 'walk dog' }, { item: 'coding' }];
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+module.exports = function (app) {
+
+    app.get('/todo', function (req, res) {
+        Todo.find({},function(err,data){
+            if(err) throw err;
+            res.render('todo', { todos: data });
+        })
+        
     });
-    res.json(data);
-});
+
+    app.post('/todo', urlencodedParser, function (req, res) {
+      var newTodo=Todo(req.body).save(function(err,data){
+        if(err) throw err;
+        res.json(data);
+      });
+    });
+
+    app.delete('/todo/:item', function (req, res) {
+        Todo.find({item: req.params.item.replace(/\-/g," ")}).remove(function(err,data){
+            if(err) throw err;
+        res.json(data);
+        });
+        
+    });
 
 };
